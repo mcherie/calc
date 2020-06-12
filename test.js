685d52ab366cd622c21ac2f6f6814ce360209d03
Cher <red_phoenix417@yahoo.com>
2020-06-11 22:52:15 -0400

    Created tests for subtraction, multiplication and division, and negative integers for addition

--- test.js
+++ test.js
@@ -2,5 +2,21 @@ const assert = require("assert")
 const operations = require("./operations.js")
 
 it("correctly calculates the sum of 1 and 3", () => {
-  assert.equal(add(1, 3), 4)
+  assert.equal(operations.add(1, 3), 4)
+})
+
+it("correctly calculates the sum of -1 and -1", () => {
+  assert.equal(operations.add(-1, -1), -2)
+})
+
+it("correctly calculates the difference of 33 and 3", () => {
+  assert.equal(operations.add(33, 3), 10)
+})
+
+it("correctly calculates the product of 12 and 12", () => {
+  assert.equal(operations.add(12, 12), 144)
+})
+
+it("correctly calculates the quotient of 10 and 2", () => {
+  assert.equal(operations.add(10, 2), 5)
 })
