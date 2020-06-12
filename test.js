8878d5ae5f6600a995a2b39b125a154ada0a6340
Cher <red_phoenix417@yahoo.com>
2020-06-11 23:24:11 -0400

    Added operations for -, *, and /

--- test.js
+++ test.js
@@ -10,13 +10,13 @@ it("correctly calculates the sum of -1 and -1", () => {
 })
 
 it("correctly calculates the difference of 33 and 3", () => {
-  assert.equal(operations.add(33, 3), 10)
+  assert.equal(operations.subtract(33, 3), 30)
 })
 
 it("correctly calculates the product of 12 and 12", () => {
-  assert.equal(operations.add(12, 12), 144)
+  assert.equal(operations.multiply(12, 12), 144)
 })
 
 it("correctly calculates the quotient of 10 and 2", () => {
-  assert.equal(operations.add(10, 2), 5)
+  assert.equal(operations.divide(10, 2), 5)
 })
